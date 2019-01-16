8072eb765f183e936ea597c3cd11829f3a49576c
Florian <flo@flotastisch.net>
2019-01-16 18:50:17 +0100

    add log.debug

--- pylgbst/comms/cpygatt.py
+++ pylgbst/comms/cpygatt.py
@@ -17,11 +17,13 @@ class GattoolConnection(Connection):
     """
 
     def __init__(self, controller='hci0'):
+        log.debug('GattoolConnection: __init__')
         Connection.__init__(self)
         self.backend = lambda: pygatt.GATTToolBackend(hci_device=controller)
         self._conn_hnd = None
 
     def connect(self, hub_mac=None):
+        log.debug('GattoolConnection: connect')
         log.debug("Trying to connect client to MoveHub with MAC: %s", hub_mac)
         adapter = self.backend()
         adapter.start()
@@ -45,20 +47,25 @@ class GattoolConnection(Connection):
         return self
 
     def disconnect(self):
+        log.debug('GattoolConnection: disconnect')
         self._conn_hnd.disconnect()
 
     def write(self, handle, data):
+        log.debug('GattoolConnection: write')
         log.debug("Writing to handle %s: %s", handle, str2hex(data))
         return self._conn_hnd.char_write_handle(handle, bytearray(data))
 
     def set_notify_handler(self, handler):
+        log.debug('GattoolConnection: set_notify_handler')
         self._conn_hnd.subscribe(MOVE_HUB_HW_UUID_CHAR, handler)
 
     def is_alive(self):
+        log.debug('GattoolConnection: is_alive')
         return True
 
 
 class BlueGigaConnection(GattoolConnection):
     def __init__(self):
+        log.debug('BlueGigaConnection: __init__')
         super(BlueGigaConnection, self).__init__()
         self.backend = lambda: pygatt.BGAPIBackend()
